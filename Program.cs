1e9ad06a4c11d0bcdfc6cf51174d1be325d83667
Anton Blazhko <blazhkoanton@gmail.com>
2026-05-13 17:11:58 +0200

    Temporary change for deployment troubleshooting

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 using echo_mate_backend.Clients;
 using echo_mate_backend.Services;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+// CORS policy for allowing requests from React development server
+var corsPolicyName = "AllowReactDev";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicyName, policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 // Add services to the container.
 builder.Services.AddScoped<TranslationService>();
@@ -10,6 +24,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddHttpClient<EchoMateEngineHttpClient>(client => 
 {
@@ -21,12 +36,18 @@ builder.Services.AddHttpClient<EchoMateEngineHttpClient>(client =>
 
 var app = builder.Build();
 
+app.UseRouting();
+
+app.UseCors("AllowReactDev");
+
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 //app.UseHttpsRedirection();
 
